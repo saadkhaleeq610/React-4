32ec8823ada8d7a5735fcafc2f77b7d3a86c860d
saadkhaleeq610 <58370495+saadkhaleeq610@users.noreply.github.com>
2020-01-21 01:00:04 +0500

    GCI-19 saad implemented endpoints

--- index.js
+++ index.js
@@ -5,7 +5,27 @@ var dataService = require('./dataService.js');
 var app = express();
 var PORT = 8080;
 
+// Routes 
+app.get('/products/all', (req, res) => {
+  res.send(Array.from(dataService.getCombinedProductMap()));
+});
 
+app.get('/products/:productId', (req, res) => {
+  let data = dataService.getCombinedProductMap();
+  res.send(data.get(req.params.productId));
+});
+
+
+app.get('/category/:ctyId', (req, res) => {
+  let data = dataService.getCombinedProductMap();
+  var filteredProducts = []
+  for (product of data) {
+    if (product[1].categoryId === req.params.ctyId) {
+        filteredProducts.push(product[1]);
+    }
+  }
+  res.send(filteredProducts);
+});
 
 
 app.listen(PORT, () => {
